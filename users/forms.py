c9805bf101cdba83a9ef90106dcb000defc30b22
SameerShiekh77 <muhammadsameer.css@gmail.com>
2023-05-17 01:27:27 +0500

    profile image work in progress

--- users/forms.py
+++ users/forms.py
@@ -10,8 +10,8 @@ class SignUpForm(UserCreationForm,forms.ModelForm):
     country = forms.CharField(label='Country', max_length=30, required=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.',widget=forms.TextInput(attrs={'class': 'border border-gray-400 py-2 px-3 rounded-lg w-full'}))
     first_name = forms.CharField(label='Username', max_length=30, required=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.',widget=forms.TextInput(attrs={'class': 'border border-gray-400 py-2 px-3 rounded-lg w-full'}))
     last_name = forms.CharField(label='Username', max_length=30, required=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.',widget=forms.TextInput(attrs={'class': 'border border-gray-400 py-2 px-3 rounded-lg w-full'}))
-
+    
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2','country','phone_number','first_name','last_name')
+        fields = ('profile_image','username', 'email', 'password1', 'password2','country','phone_number','first_name','last_name')
         extra_kwargs = {'password1': {'write_only': True}, 'password2': {'write_only': True}, 'email':{'unique':True}}
--- users/models.py
+++ users/models.py
@@ -6,6 +6,6 @@ class User(AbstractUser):
     country = models.CharField(max_length=50, blank=True)
     phone_number = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=50, blank=True)
-
+    profile_image = models.ImageField(upload_to='users/profile', blank=True)
     def __str__(self):
         return self.username
